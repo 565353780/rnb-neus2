cbb38ab31a9948fa50d2f34c5e740b4c56b9af0f
chLi <565353780@qq.com>
2026-01-21 10:21:53 +0800

    create setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,12 +1,14 @@
-cd ..
-git clone https://github.com/RobinBruneau/RNb-NeuS2.git rnb2
-
 conda install -c conda-forge xorg-libxrandr xorg-libxinerama \
-  xorg-libxcursor xorg-libxi glew mesalib
+  xorg-libxcursor xorg-libxi glew mesalib -y
+conda install -c anaconda mesa-libgl-cos6-x86_64 -y
+conda install -c menpo glfw3 -y
 
-cd rnb2
+pip install ninja
 
-pip install -r requirements.txt
+pip install commentjson contourpy cycler Cython fonttools joblib \
+  kiwisolver lark-parser matplotlib numpy opencv-python packaging \
+  pillow pybind11 pyembree pyparsing python-dateutil rtree \
+  scikit-learn scipy six threadpoolctl tqdm trimesh
 
 rm -rf build
 
